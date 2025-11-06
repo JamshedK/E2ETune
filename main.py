8a700d3f64259cf28b43cd8435ec6756ee112a30
= <=>
2025-11-06 19:28:30 +0000

    Refactor workload handling and remove unused argparse code

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-import argparse
 from config import parse_config
 import os
 from tune import tune
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='localhost', help='the database host')
-    parser.add_argument('--database', type=str, default='benchbase', help='workload file')
-    cmd = parser.parse_args()
-
     # Load configuration file
     args = parse_config.parse_args("config/config.ini")
-    # print(args)
-    # args['benchmark_config']['workload_path'] = 'SuperWG/res/gpt_workloads/' + cmd.workload
-    args['database_config']['database'] = cmd.database
-    args['tuning_config']['offline_sample'] += cmd.host
     print(args)
 
-    all = os.listdir('./olap_workloads')
-    workload_type = 'tpch'
-    workloads = [i for i in all if i.startswith(workload_type)]
+    # Get workload path from config
+    workload_base_path = args['benchmark_config']['workload_path']
+    workload_type = args['benchmark_config']['benchmark']
+    benchmark_type = args['benchmark_config']['type']
+    
+    all = os.listdir('./' + workload_base_path + '/')
+    
+    # Filter based on benchmark type
+    if benchmark_type == 'oltp':
+        workloads = sorted([i for i in all if i.startswith(f'sample_{workload_type}_config') and i.endswith('.xml') and i != f'sample_{workload_type}_config.xml'])
+    else:
+        workloads = sorted([i for i in all if i.startswith(workload_type)])
 
     for idx in range(0, 1):
-        print("Begin tuning for workload: ", idx)
-        args['benchmark_config']['workload_path'] = './olap_workloads/' + workloads[idx]
+        print("Begin tuning for workload:", idx)
+        full_workload_path = os.path.join('./', workload_base_path, workloads[idx])
+        print(f'tune for workload: {full_workload_path}')
         try:
-            tune(workload_file=workloads[idx], args=args)
+            tune(workload_file=full_workload_path, args=args)
             break
-            # print(f'train surrogate model for {workloads[idx]}')
         except Exception as e:
             print(f'occur {e}')
             continue
         break
-    
